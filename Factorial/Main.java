20f21d8495870624dfa970580bf5742271746c04
mcmunchie <89753143+mcmunchie@users.noreply.github.com>
2022-05-17 09:22:01 -0500

    Create Main.java

---
+++ Factorial/Main.java
@@ -0,0 +1,8 @@
+package Factorial;
+
+public class Main {
+  public static void main(String[] args) {
+    Factorial product = new Factorial(); // constructor, product object of Factorial class
+    System.out.println("Factorial: " + product.fact(5));
+  }
+}
