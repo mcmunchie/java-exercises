65edfd9fdceec78354e307b7b960ba6865ab7312
mcmunchie <89753143+mcmunchie@users.noreply.github.com>
2022-05-17 10:43:29 -0500

    Create EndlessRecursion.java

---
+++ Recursion/EndlessRecursion.java
@@ -0,0 +1,10 @@
+package Recursion;
+
+public class EndlessRecursion {
+  public static void message(int n) {
+    if (n > 0) {
+      System.out.println("This is a recursive method.");
+      message(n - 1);
+    }
+  }
+}
